42e7d8766983c56e1f87783229dc4a469ee2b9fc
hama1185 <hamataku7332@softbank.ne.jp>
2020-04-11 02:58:01 +0900

    jsonから要素を抽出

---
+++ Assets/Scripts/COVIDList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class COVIDList
+{
+    // Start is called before the first frame update
+    
+    public string srcurl_pdf;
+    public string srcurl_web;
+    public string description;
+    public string lastUpdate;
+    public int npatients;//stringのほういいかも
+    public int nexits;
+    public int ndeaths;
+    public int ncurrentpatients;
+    [System.Serializable]
+    public struct areaList{
+        public string name;
+        public string name_jp;
+        public int npatients;
+        public int ncurrentpatients;
+        public int nexits;
+        public int ndeaths;
+    }
+    public areaList[] area;
+
+    public void print()
+    {
+        foreach(var item in area)
+        {
+            Debug.Log("[Name:" + item.name +
+                      "][namejp:" + item.name_jp +
+                      "][npatients:" + item.npatients +
+                      "][ncurrentpatients:" + item.ncurrentpatients + 
+                      "][nexits:" + item.nexits +
+                      "][ndeaths:" + item.ndeaths + "]");
+        }
+    }
+}
---
+++ Assets/Scripts/GetWeb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using MiniJSON;
+public class GetWeb : MonoBehaviour{
+    // Start is called before the first frame update
+    public void onClick(){
+        StartCoroutine(getWeb());
+    }
+
+    IEnumerator getWeb(){
+        //リクエスト文
+        var webRequest = UnityWebRequest.Get("https://app.sabae.cc/api/covid19japan.json");
+        //通信
+        yield return webRequest.SendWebRequest();
+        //エラー検出
+        if(webRequest.isHttpError || webRequest.isNetworkError){
+            Debug.Log(webRequest.error);
+        }
+        switch(webRequest.downloadProgress){
+            case 1:
+                COVIDList data = JsonUtility.FromJson<COVIDList>(webRequest.downloadHandler.text);
+                Debug.Log(data.area[0].name);
+                break;
+            case 0.5f:
+                Debug.Log("connecting... downloadHandler is null");
+                break;
+            case -1:
+                Debug.Log("Send is no call");
+                break;
+                
+        }
+    }
+    
+}
