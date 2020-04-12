b1c246fbd945fde495b3a53e9c1dcb9eaf300316
hama1185 <hamataku7332@softbank.ne.jp>
2020-04-12 23:48:31 +0900

    マーカーを認識したら物体を表示する

---
+++ Assets/Scripts/ARImage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ARImage : MonoBehaviour
+{
+    GameObject japan = null;
+    public GameObject PrefabJapan;
+    private GameObject Japan = null;
+    public GameObject WebManager;
+    GetWeb getWeb;
+    // Start is called before the first frame update
+    void Awake() {
+        Application.targetFrameRate = 60;    
+    }
+    void Start() {
+        getWeb = WebManager.GetComponent<GetWeb>();
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update(){
+        List<AugmentedImage> markers = new List<AugmentedImage>();
+        if(Session.Status != SessionStatus.Tracking){
+            return;
+        }
+        Session.GetTrackables<AugmentedImage>(markers, TrackableQueryFilter.Updated);
+        foreach(AugmentedImage marker in markers){
+            if(marker.TrackingState == TrackingState.Tracking){
+                if(Japan == null){
+                    Anchor anchor = marker.CreateAnchor(marker.CenterPose);
+                    Japan = Instantiate(PrefabJapan, anchor.transform);
+                }
+                else{
+                    Japan.transform.position = marker.CenterPose.position;
+                    Japan.transform.rotation = marker.CenterPose.rotation;
+                }
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UIManage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text UpdateTime;
+    public GameObject WebManager;
+    GetWeb getWeb;
+    void Start(){
+        getWeb = WebManager.GetComponent<GetWeb>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        UpdateTime.text = "This data is for" + getWeb.data.lastUpdate;
+    }
+}
